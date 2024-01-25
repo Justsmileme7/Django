f949d06c9e074b7e2c6e24dafc4c7c38c30793c7
evgenia <smallbee_@mail.ru>
2024-01-25 20:19:07 +0300

    delate

--- list_of_auto/site_of_auto/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Auto,Automodel, Brand, UserCar
-
-# Register your models here.
-admin.site.register(Auto)
-admin.site.register(Automodel)
-admin.site.register(Brand)
-admin.site.register(UserCar)
