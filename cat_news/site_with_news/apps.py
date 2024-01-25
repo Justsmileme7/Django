71bc486205faa223090d5aab147611586be8157e
evgenia <smallbee_@mail.ru>
2024-01-25 21:59:01 +0300

    create new project cat_news

---
+++ cat_news/site_with_news/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SiteWithNewsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'site_with_news'
---
+++ cat_news/site_with_news/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+class News(models.Model):
+    image = models.ImageField()
+    title = models.CharField(max_length=50, unique=True)
+    context = models.TextField()
+    rating = models.IntegerField(default=0)
+    rating_popular = models.IntegerField(default=0)
+    date_of_create = models.DateTimeField()
+    author = models.CharField(max_length=20)
+
+class Comment(models.Model):
+    nickname = models.CharField(max_length=10)
+    context_of_comment = models.TextField()
+    like = models.IntegerField()
+    dislike = models.IntegerField()
+    date_of_create_comment = models.DateTimeField()
+    news_id = models.ForeignKey(News, on_delete=models.CASCADE)
+    comment_id = models.ForeignKey('Comment', on_delete=models.CASCADE)
