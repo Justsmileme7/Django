b5fd540c789af2a47631ceff54eea45c9e5e933c
evgenia <smallbee_@mail.ru>
2024-01-30 22:10:45 +0300

    create site without logo

--- list_of_auto/site_of_auto/admin.py
+++ list_of_auto/site_of_auto/admin.py
@@ -1,8 +1,38 @@
 from django.contrib import admin
-from .models import Auto,Automodel, Brand, UserCar
+from django.contrib.admin import ModelAdmin
+from django.utils.safestring import mark_safe
+
+from .models import Auto, Automodel, Brand, UserCar
 
 # Register your models here.
-admin.site.register(Auto)
-admin.site.register(Automodel)
-admin.site.register(Brand)
-admin.site.register(UserCar)
+
+@admin.register(Auto)
+class AutoAdmin(admin.ModelAdmin):
+    list_display = ('autobrand', 'automodel', 'status', 'vin_code')
+    list_filter = ('status', 'autobrand')
+
+
+@admin.register(Automodel)
+class AutomodelAdmin(admin.ModelAdmin):
+    list_display = ('autobrand', 'automodel')
+    list_filter = ('autobrand',)
+    search_fields = ('vin_code',)
+
+    def count_auto(self, instance):
+        count = Auto.objects.filter(automodel__name=instance.name).count()
+        return count
+
+@admin.register(UserCar)
+class UserCarAdmin(admin.ModelAdmin):
+    list_display = ('username', 'user_mail', 'user_phone_number')
+    search_fields = ('user_phone_number',)
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('preview', 'name')
+
+    def preview(self, instance: Brand):
+        if instance.image_brand :
+            return mark_safe(f'img scr="{instance.image_brand .url}" alt="">')
+        return mark_safe('Without logo')
+
--- list_of_auto/site_of_auto/models.py
+++ list_of_auto/site_of_auto/models.py
@@ -11,30 +11,29 @@ class Auto(models.Model):
     autobrand = models.ForeignKey('Brand', on_delete=models.CASCADE)
     automodel = models.ForeignKey('Automodel', on_delete=models.CASCADE)
     vin_code = models.CharField(max_length=255)
-    user = models.ForeignKey('UserCar', on_delete=models.CASCADE)
 
 
 class Automodel(models.Model):
     automodel = models.CharField(max_length=255)
     autobrand = models.ForeignKey('Brand', on_delete=models.CASCADE)
 
-
+    def __str__(self):
+        return self.automodel
 
 
 class Brand(models.Model):
     name = models.CharField(max_length=255)
-    image_brand = models.ImageField()
+    image_brand = models.ImageField(null=True, blank=True, default=None, upload_to='logos')
+
+    def __str__(self):
+        return self.name
 
 
 class UserCar(models.Model):
     username = models.CharField(max_length=255)
     user_mail = models.EmailField()
     user_phone_number = models.CharField(max_length=12)
+    auto = models.OneToOneField('Auto', on_delete=models.SET_NULL, null=True, default=None)
 
-
-
-
-
-
-
-
+    def __str__(self):
+        return self.username
