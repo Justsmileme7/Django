87163ff672e738070523b89397a32d0b683e4355
evgenia <smallbee_@mail.ru>
2024-02-01 22:12:58 +0300

    write title

--- cat_news/site_with_news/views.py
+++ cat_news/site_with_news/views.py
@@ -4,5 +4,6 @@ from django.views import View
 
 # Create your views here.
 class MainPageView(View):
+    PAGE_TITLE = 'Main Page'
     def get(self, request):
-        return render(request, 'mainpage.html')
+        return render(request, 'mainpage.html', context={"title": self.PAGE_TITLE})
