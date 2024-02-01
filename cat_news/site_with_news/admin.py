735d074004e353ce4e7d1bf6b6167546a2619b25
evgenia <smallbee_@mail.ru>
2024-02-01 20:16:57 +0300

    add admin

--- cat_news/site_with_news/admin.py
+++ cat_news/site_with_news/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from django.utils.safestring import mark_safe
+
+from site_with_news.models import News, Comment
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'image', 'context', 'rating')
+    search_fields = ('title', 'context')
+    ordering = ('title', 'rating')
+
+
+    def preview(self, instance: News):
+        if instance.picture:
+            return mark_safe(f'<img style="max-width: 30px" src="{instance.picture.url}" alt="">')
+        return mark_safe('Without picture')
+
