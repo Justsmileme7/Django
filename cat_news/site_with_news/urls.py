3dab2bd0e38e0e3b5792a95db156f202859ce24d
evgenia <smallbee_@mail.ru>
2024-02-10 22:53:03 +0300

    Create main page and pages with news

--- cat_news/site_with_news/urls.py
+++ cat_news/site_with_news/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, re_path
-from .views import MainPageView
+from .views import MainPageView, NewsPageView
 
 urlpatterns = [
     path('', MainPageView.as_view(), name='mainpage'),
+    path('/news/<int:id>/', NewsPageView.as_view(), name='newspage'),
 
 ]
--- cat_news/site_with_news/views.py
+++ cat_news/site_with_news/views.py
@@ -8,7 +8,10 @@ class MainPageView(View):
     PAGE_TITLE = 'Cats news'
 
     def get(self, request, *args, **kwargs):
-        news = News.objects.all()  # достаем все объекты из базы данных из таблицы news
+        sort_by = request.GET.get('sort', 'date_of_create')  # получаем из нтмл переменную sort
+        order_by = request.GET.get('order', 'back_order')
+        sort_by = sort_by if order_by == 'wright_order' else f'-{sort_by}'
+        news = News.objects.order_by(sort_by)  # достаем отсортированные  объекты из базы данных из таблицы news
         for new in news:
             list_of_news = new.context.split(' ')[:10]
             description = ' '.join(list_of_news)
@@ -16,6 +19,17 @@ class MainPageView(View):
         return render(request, 'mainpage.html',
                       context={"title": self.PAGE_TITLE,
                                'news_list': news,
+                               })
+
+
+class NewsPageView(View):
+
+    def get(self, request, id, *args, **kwargs):
+        news = News.objects.get(id=id)  # достаем одну новость по ИД из базы данных из таблицы news
+        title = news.title
+        return render(request, 'newspage.html',
+                      context={"title": title,
+                               'news': news,
                                })
 
 
